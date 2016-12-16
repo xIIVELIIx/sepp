7e146dfe4d5b9cc2a716b8ba1a397cb174964e8c
jmalaver1712 <jhonatan.jjma@gmail.com>
2016-12-16 15:24:00 -0500

    Interfaz Base
    Formulario
    Tabla de Datos

---
+++ application/views/plantilla/content.php
@@ -0,0 +1,238 @@
+ <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Dashboard
+            <small>Control panel</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+          </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+          <!-- Main row -->
+          <div class="row">
+
+            <!-- Left col -->
+            <section class="col-lg-5 connectedSortable">
+
+              <div class="box box-danger">
+
+              <div class="box-header">
+                  <h3 class="box-title">Forms</h3>
+                  <div class="box-tools pull-right" data-toggle="tooltip" title="Status">
+                    <div class="box-tools pull-right">
+                      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                      <!-- <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button> -->
+                  </div>
+                  </div>
+                </div>
+                <div class="box-body">
+
+                  <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                    <input type="email" class="form-control" placeholder="Email">
+                  </div>
+
+                  <!-- Date dd/mm/yyyy -->
+                  <div class="form-group">
+                    <label>Date masks:</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+                      <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
+                    </div><!-- /.input group -->
+                  </div><!-- /.form group -->
+
+
+                  <!-- phone mask -->
+                  <div class="form-group">
+                    <label>US phone mask:</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-phone"></i>
+                      </div>
+                      <input type="text" class="form-control" data-inputmask='"mask": "(999) 999-9999"' data-mask>
+                    </div><!-- /.input group -->
+                  </div><!-- /.form group -->
+
+
+                  <div class="form-group">
+                    <label>Minimal</label>
+                    <select class="form-control select2" style="width: 100%;">
+                      <option selected="selected">Alabama</option>
+                      <option>Alaska</option>
+                      <option>California</option>
+                      <option>Delaware</option>
+                      <option>Tennessee</option>
+                      <option>Texas</option>
+                      <option>Washington</option>
+                    </select>
+                  </div><!-- /.form-group -->
+
+                  <div class="form-group">
+                      <label>Textarea</label>
+                      <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                  </div>
+
+
+                </div><!-- /.box-body -->
+                <div class="box-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+
+              </div><!-- /.box -->
+
+            </section>
+            <!-- Left col -->
+
+            <!-- right col -->
+            <section class="col-lg-7 connectedSortable">
+
+              <!-- Chat box -->
+              <div class="box box-success">
+                <div class="box-header">
+                  <i class="fa fa-comments-o"></i>
+                  <h3 class="box-title">Tabla Prueba</h3>
+                  <div class="box-tools pull-right" data-toggle="tooltip" title="Status">
+                    <div class="box-tools pull-right">
+                      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                      <!-- <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button> -->
+                  </div>
+                  </div>
+                </div>
+                <div class="box-body chat" id="chat-box">
+
+                  <table id="data_table1" class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th>Rendering engine</th>
+                        <th>Browser</th>
+                        <th>Platform(s)</th>
+                        <th>Engine version</th>
+                        <th>CSS grade</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td> 4</td>
+                        <td>X</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 5.0</td>
+                        <td>Win 95+</td>
+                        <td>5</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 5.5</td>
+                        <td>Win 95+</td>
+                        <td>5.5</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 6</td>
+                        <td>Win 98+</td>
+                        <td>6</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td> 4</td>
+                        <td>X</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 5.0</td>
+                        <td>Win 95+</td>
+                        <td>5</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 5.5</td>
+                        <td>Win 95+</td>
+                        <td>5.5</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 6</td>
+                        <td>Win 98+</td>
+                        <td>6</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td> 4</td>
+                        <td>X</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 5.0</td>
+                        <td>Win 95+</td>
+                        <td>5</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 5.5</td>
+                        <td>Win 95+</td>
+                        <td>5.5</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet
+                          Explorer 6</td>
+                        <td>Win 98+</td>
+                        <td>6</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Trident</td>
+                        <td>Internet Explorer 7</td>
+                        <td>Win XP SP2+</td>
+                        <td>7</td>
+                        <td>A</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div><!-- /.chat -->
+                <div class="box-footer">
+                  
+                </div>
+              </div><!-- /.box (chat box) -->
+            </section>
+            <!-- right col -->
+           
+          </div><!-- /.row (main row) -->
+
+        </section><!-- /.content -->
+      </div><!-- /.content-wrapper -->
