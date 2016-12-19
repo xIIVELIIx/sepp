59811577ff9787821c1f27dc7fb948e5cf14a878
jmalaver1712 <jhonatan.jjma@gmail.com>
2016-12-19 09:09:30 -0500

    Interfaz V2
    Seccion Izquierda Separada

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -9,10 +9,12 @@ class Inicio extends CI_Controller
 
 		//$this->db->get('ciudad'); 
 		$data = $arrayName = array('titulo' => 'SEPP');
+
 		$this->load->view("plantilla/head", $data);
 		$this->load->view("plantilla/header");
 		$this->load->view("plantilla/nav");
 		$this->load->view("plantilla/content");
+		$this->load->view("plantilla/nav_right");
 		$this->load->view("plantilla/footer");
 
 		// print("Hola Mundo");
--- application/views/plantilla/header.php
+++ application/views/plantilla/header.php
@@ -5,16 +5,19 @@
         <!-- Logo -->
         <a href="#" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>A</b>LT</span>
+          <span class="logo-mini"><b>SEPP</b></span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Admin</b>LTE</span>
+          <span class="logo-lg"><b>SEPP</b></span>
         </a>
+        
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
+
           <!-- Sidebar toggle button-->
           <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
           </a>
+
           <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
               <!-- Messages: style can be found in dropdown.less-->
